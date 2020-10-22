ca9e222d51648ebe132eba2b10d181157adcc22b
poojachirp <poojasrinath@gmail.com>
2020-10-21 20:20:01 -0400

    ignoring src/Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const appConfig = {
-    username: "",
-    password: ""
+    username: "SK53060506846e5aba7246ff78939dff61",
+    password: "RGj2inUIPhXL5ECOPTSJjrk2gAiy9IIW"
 }
 
 export  default  appConfig
